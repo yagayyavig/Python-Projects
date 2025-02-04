e94fa8610b5d4a52bb03ce24a654802b5776c1d8
yagayyavig <yagayyavig@gmail.com>
2025-02-03 22:46:55 -0800

    Easy Projects: Shopping Cart and Area Calc

---
+++ Easy projects/Rectangle Area Calculator.py
@@ -0,0 +1,8 @@
+# Rectangle Area Calc
+
+length = float(input("Enter the Length: "))
+width = float(input("Enter the Width: "))
+
+area = length * width
+print(f" The area is: {area}cm²")
+
---
+++ Easy projects/Shoopping Cart.py
@@ -0,0 +1,10 @@
+# Shopping cart 
+
+item = input("What item would you like to buy: ")
+price = float(input("What is the price: "))
+quantity = int(input("How man would you like: "))
+
+
+total = price * quantity
+print(f" You have bought{quantity} x {item}/s")
+print(f"Your total is: ${total}")
